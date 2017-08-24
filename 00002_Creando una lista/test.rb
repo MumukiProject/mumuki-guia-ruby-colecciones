e8eeaea5e01f3522c04d6ad773a5c47788c2c3ea
franco@mumuki.org <franco@mumuki.org>
2017-08-24 00:18:38 -0400

    Mumuki Export on 2017-08-24 00:18:38 -0400

--- 00002_Creando una lista/test.rb
+++ 00002_Creando una lista/test.rb
@@ -1,1 +1,11 @@
-todo
+it "la biblioteca debe contener al CarlosDuty"  do
+  expect(biblioteca.juegos.include?(CarlosDuty)).to be true
+end
+
+it "la biblioteca debe contener al TimaElLeon"  do
+  expect(biblioteca.juegos.include?(TimaElLeon)).to be true
+end
+
+it "la biblioteca NO debe contener al DevilMayLaugh"  do
+  expect(biblioteca.juegos.include?(DevilMayLaugh)).to be false
+end
