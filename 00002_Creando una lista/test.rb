1621b546e2ce91b05a0b81cd8e47315e1440ac62
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2017-12-28 19:37:48 -0500

    Mumuki Export on 2017-12-28 19:37:48 -0500

--- 00002_Creando una lista/test.rb
+++ 00002_Creando una lista/test.rb
@@ -1,18 +1,15 @@
 it "El primer juego de la la Biblioteca es el CarlosDuty" do
-  Biblioteca.juegos.pop
-  Biblioteca.juegos.pop
-  expect(Biblioteca.juegos.pop).to be CarlosDuty
+  expect(Biblioteca.juegos.shift).to be CarlosDuty
 end
 
 it "El segundo juego de la Biblioteca es TimbaElLeon"  do
-  Biblioteca.juegos.pop
-  expect(Biblioteca.juegos.pop.to be TimbaElLeon
+  expect(Biblioteca.juegos.shift).to be TimbaElLeon
 end
 
 it "El tercer juego de la Biblioteca es TimbaElLeon"  do
-  expect(Biblioteca.juegos.pop).to be TimbaElLeon
+  expect(Biblioteca.juegos.shift).to be TimbaElLeon
 end
 
 it "La Biblioteca tiene únicamente esos tres juegos"  do
-  expect(Biblioteca.juegos).to be [CarlosDuty, TimbaElLeon, TimbaElLeon]
+  expect(Biblioteca.juegos.empty?).to be true
 end
