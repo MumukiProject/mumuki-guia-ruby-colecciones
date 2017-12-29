c35420b15162c266f201cf2bde14c6340ff724af
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2017-12-28 22:17:39 -0500

    Mumuki Export on 2017-12-28 22:17:39 -0500

--- 00003_Algunos mensajes básicos/test.rb
+++ 00003_Algunos mensajes básicos/test.rb
@@ -1,38 +1,38 @@
 it "Si se adquiere Metroide se agrega a los juegos y suma puntos"  do
-  biblioteca.adquirir_juego!(Metroide)
-  expect(biblioteca.juegos.include?(Metroide)).to be true
-  expect(biblioteca.puntos()).to eq 150
+  Biblioteca.adquirir_juego!(Metroide)
+  expect(Biblioteca.juegos.include?(Metroide)).to be true
+  expect(Biblioteca.puntos()).to eq 150
 end
 
 it "Si se borra CarlosDuty la biblioteca queda con dos juegos" do
-  biblioteca.borrarJuego(CarlosDuty)
-  expect(biblioteca.juegos.include?(CarlosDuty)).to be false
-  expect( biblioteca.juegos.size()).to eq 2
-  expect( biblioteca.puntos()).to eq 0
+  Biblioteca.borrarJuego(CarlosDuty)
+  expect(Biblioteca.juegos.include?(CarlosDuty)).to be false
+  expect(Biblioteca.juegos.size()).to eq 2
+  expect(Biblioteca.puntos()).to eq 0
 end
 
 it "La Biblioteca no es completa al iniciar" do
-  expect(biblioteca.completa?).to be false
+  expect(Biblioteca.completa?).to be false
 end
 
 it "Si se adquieren suficientes juegos la Biblioteca es completa" do
-  biblioteca.adquirir_juego!(Metroide)
-  biblioteca.adquirir_juego!(Metroide.new)
-  biblioteca.adquirir_juego!(Metroide.new)
-  biblioteca.adquirir_juego!(Metroide.new)
-  biblioteca.adquirir_juego!(Metroide.new)
-  expect(biblioteca.completa?).to be false
-  biblioteca.adquirir_juego!(Metroide.new)
-  biblioteca.adquirir_juego!(Metroide.new)
-  expect(biblioteca.completa?).to be true
+  Biblioteca.adquirir_juego!(Metroide)
+  Biblioteca.adquirir_juego!(Metroide.new)
+  Biblioteca.adquirir_juego!(Metroide.new)
+  Biblioteca.adquirir_juego!(Metroide.new)
+  Biblioteca.adquirir_juego!(Metroide.new)
+  expect(Biblioteca.completa?).to be false
+  Biblioteca.adquirir_juego!(Metroide.new)
+  Biblioteca.adquirir_juego!(Metroide.new)
+  expect(Biblioteca.completa?).to be true
 end
 
 it "CarlosDuty es recomendable si no está en la biblioteca" do
-  expect(biblioteca.esJuegoRecomendable(CarlosDuty)).to be false
-  biblioteca.borrarJuego(CarlosDuty)
-  expect(biblioteca.esJuegoRecomendable(CarlosDuty)).to be true
+  expect(Biblioteca.esJuegoRecomendable(CarlosDuty)).to be false
+  Biblioteca.borrarJuego(CarlosDuty)
+  expect(Biblioteca.esJuegoRecomendable(CarlosDuty)).to be true
 end
 
 it "TimbaElLeon no es recomendable porque no es violento" do
-  expect(biblioteca.esJuegoRecomendable(TimbaElLeon)).to be false
+  expect(Biblioteca.esJuegoRecomendable(TimbaElLeon)).to be false
 end
