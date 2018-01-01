6caf714ac45a13640472fb6a5581e4be5f82a400
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-01 17:59:01 -0500

    Mumuki Export on 2018-01-01 17:59:01 -0500

--- 00012_Jugando a todo/default.rb
+++ 00012_Jugando a todo/default.rb
@@ -1,11 +1,1 @@
-module Biblioteca
-  @juegos = [CarlosDuty, TimbaElLeon, TimbaElLeon]
-  @puntos = 0
-  
-  def self.juegos
-    @juegos
-  end
-  
-  #y más métodos que no hacen falta ahora...
-  
-end
+#...solution[2]...#
