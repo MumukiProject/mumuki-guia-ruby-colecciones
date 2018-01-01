de8b108f54f09daca43b554359513c3441371615
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-01 17:51:09 -0500

    Mumuki Export on 2018-01-01 17:51:09 -0500

--- 00010_El viejo y querido map/test.rb
+++ 00010_El viejo y querido map/test.rb
@@ -2,11 +2,11 @@ it "Los juegos de la Biblioteca son CarlosDuty, TimbaElLeon y Metroide"  do
   expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
 end
 
-it "La dificultad violenta de los juegos violentos es 30 y 100"  do
-  expect(Biblioteca.dificultad_violenta).to eq [30, 100]
+it "La dificultad violenta del único juego violento es 30"  do
+  expect(Biblioteca.dificultad_violenta).to eq [30]
 end
 
-it "Si CarlosDuty no estuviera en la Biblioteca sólo queda la dificultad violenta de Metroide"  do
+it "Si CarlosDuty no estuviera en la Biblioteca no habría dificultad violenta"  do
   Biblioteca.juegos.shift
-  expect(Biblioteca.dificultad_violenta).to eq [100]
+  expect(Biblioteca.dificultad_violenta).to eq []
 end
