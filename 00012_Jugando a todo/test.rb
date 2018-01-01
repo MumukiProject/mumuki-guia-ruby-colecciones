672f422a932291f49c46882a8cddabe0556825c5
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-01 17:26:44 -0500

    Mumuki Export on 2018-01-01 17:26:44 -0500

--- 00012_Jugando a todo/test.rb
+++ 00012_Jugando a todo/test.rb
@@ -5,5 +5,5 @@ end
 it "Jugar a todo baja un punto la dificultad a CarlosDuty y sube en 5 la de TimbaElLeon"  do
   Biblioteca.jugar_a_todo!
   expect(CarlosDuty.dificultad).to eq 29.5
-  expect(TimbaElLeon.dificultad). to eq 25
+  expect(TimbaElLeon.dificultad). to eq 30
 end
