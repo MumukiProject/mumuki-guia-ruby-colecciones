93c5068676bcebffbd78aa483312f6bc5db73700
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-01 17:39:33 -0500

    Mumuki Export on 2018-01-01 17:39:33 -0500

--- 00006_Bloques con parámetros/extra.rb
+++ 00006_Bloques con parámetros/extra.rb
@@ -1,11 +1,1 @@
-module TimbaElLeon
-  @dificultad = 25
-
-  def self.jugar!(un_tiempo)
-    @dificultad += un_tiempo
-  end
-  
-  def self.dificultad
-    @dificultad
-  end
-end
+#...solution[1]...#
