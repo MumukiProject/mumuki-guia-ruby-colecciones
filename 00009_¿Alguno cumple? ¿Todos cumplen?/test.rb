22c89d07d14e54bdc094dd4ed0d507b311d97571
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-01 17:47:11 -0500

    Mumuki Export on 2018-01-01 17:47:11 -0500

--- 00009_¿Alguno cumple? ¿Todos cumplen?/test.rb
+++ 00009_¿Alguno cumple? ¿Todos cumplen?/test.rb
@@ -1,11 +1,11 @@
-it "Los juegos de la Biblioteca son CarlosDuty y dos copias de TimbaElLeon"  do
-  expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, TimbaElLeon]
+it "Los juegos de la Biblioteca son CarlosDuty, TimbaElLeon y Metroide"  do
+  expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
 end
 
 it "En la Biblioteca no hay mucha violencia porque TimbaElLeon no es violento"  do
   expect(Biblioteca.mucha_violencia?).to be false
 end
 
-it "La Biblioteca es muy difícil porque CarlosDuty tiene más de 25 puntos de dificultad"  do
+it "La Biblioteca es muy difícil porque CarlosDuty y Metroide tienen más de 25 puntos de dificultad"  do
   expect(Biblioteca.muy_dificil?).to eq true
 end
