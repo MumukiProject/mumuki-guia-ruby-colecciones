9b69cdbe6fd8c1f79f561f7071714fd279c05239
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-04 21:57:19 -0500

    Mumuki Export on 2018-01-04 21:57:19 -0500

--- 00006_Bloques con parámetros/test.rb
+++ 00006_Bloques con parámetros/test.rb
@@ -1,4 +1,4 @@
-it "Si se evalúa jugar_TimbaElLeon con 10 horas, la dificultad es 35"  do
+it "Si se evalúa jugar_a_timba con 10 horas, la dificultad es 35"  do
   jugar_a_timba.call(10)
   expect(TimbaElLeon.dificultad).to be 35
 end
