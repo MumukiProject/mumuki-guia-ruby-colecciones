1b7f353327e508f625bf216370abf81e7f6c8d0a
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-01 18:23:04 +0000

    Mumuki Export on 2021-02-01 18:23:04 +0000

--- 00003_Mejorando la Biblioteca/hint.md
+++ 00003_Mejorando la Biblioteca/hint.md
@@ -1,4 +1,4 @@
-Para saber si la `Biblioteca` es `completa?` necesitás que se cumplan dos condiciones a la vez: que tenga más de 1000 puntos **y** más de 5 juegos. Y para `juego_recomendable?` necesitás que el juego **no esté** en la `Biblioteca`. Quizá `&&` y `!` te sean útiles en estos dos casos:
+Para saber si la `Juegoteca` es `completa?` necesitás que se cumplan dos condiciones a la vez: que tenga más de 1000 puntos **y** más de 5 juegos. Y para `juego_recomendable?` necesitás que el juego **no esté** en la `Juegoteca`. Quizá `&&` y `!` te sean útiles en estos dos casos:
 
 ```ruby
 ム 7 > 3 && Mario.hermano_de? Luigi
--- 00003_Mejorando la Biblioteca/test.rb
+++ 00003_Mejorando la Biblioteca/test.rb
@@ -1,45 +1,45 @@
-it "Los juegos de la Biblioteca son CarlosDuty, TimbaElLeon y Metroide"  do
-  expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
+it "Los juegos de la Juegoteca son CarlosDuty, TimbaElLeon y Metroide"  do
+  expect(Juegoteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
 end
 
-it "Si se borra CarlosDuty la Biblioteca queda con dos juegos" do
-  Biblioteca.borrar_juego!(CarlosDuty)
-  expect(Biblioteca.juegos.include?(CarlosDuty)).to be false
-  expect(Biblioteca.juegos.size()).to eq 2
-  expect(Biblioteca.puntos()).to eq 0
-  Biblioteca.juegos.push(CarlosDuty)
+it "Si se borra CarlosDuty la Juegoteca queda con dos juegos" do
+  Juegoteca.borrar_juego!(CarlosDuty)
+  expect(Juegoteca.juegos.include?(CarlosDuty)).to be false
+  expect(Juegoteca.juegos.size()).to eq 2
+  expect(Juegoteca.puntos()).to eq 0
+  Juegoteca.juegos.push(CarlosDuty)
 end
 
 it "Si se adquiere otro juego se agrega a los juegos y suma puntos"  do
-  Biblioteca.adquirir_juego!(OtroJuego)
-  expect(Biblioteca.juegos.include?(OtroJuego)).to be true
-  expect(Biblioteca.puntos()).to eq 150
+  Juegoteca.adquirir_juego!(OtroJuego)
+  expect(Juegoteca.juegos.include?(OtroJuego)).to be true
+  expect(Juegoteca.puntos()).to eq 150
 end
 
-it "La Biblioteca no es completa al iniciar" do
-  expect(Biblioteca.completa?).to be false
+it "La Juegoteca no es completa al iniciar" do
+  expect(Juegoteca.completa?).to be false
 end
 
-it "Si se adquieren suficientes juegos la Biblioteca es completa" do
-  Biblioteca.adquirir_juego!(OtroJuego)
-  Biblioteca.adquirir_juego!(OtroJuego)
-  Biblioteca.adquirir_juego!(OtroJuego)
-  Biblioteca.adquirir_juego!(OtroJuego)
-  Biblioteca.adquirir_juego!(OtroJuego)
-  expect(Biblioteca.completa?).to be false
-  Biblioteca.adquirir_juego!(OtroJuego)
-  Biblioteca.adquirir_juego!(OtroJuego)
-  expect(Biblioteca.completa?).to be true
-  7.times { Biblioteca.juegos.delete(OtroJuego) } 
+it "Si se adquieren suficientes juegos la Juegoteca es completa" do
+  Juegoteca.adquirir_juego!(OtroJuego)
+  Juegoteca.adquirir_juego!(OtroJuego)
+  Juegoteca.adquirir_juego!(OtroJuego)
+  Juegoteca.adquirir_juego!(OtroJuego)
+  Juegoteca.adquirir_juego!(OtroJuego)
+  expect(Juegoteca.completa?).to be false
+  Juegoteca.adquirir_juego!(OtroJuego)
+  Juegoteca.adquirir_juego!(OtroJuego)
+  expect(Juegoteca.completa?).to be true
+  7.times { Juegoteca.juegos.delete(OtroJuego) } 
 end
 
-it "CarlosDuty es recomendable si no está en la biblioteca" do
-  expect(Biblioteca.juego_recomendable?(CarlosDuty)).to be false
-  Biblioteca.borrar_juego!(CarlosDuty)
-  expect(Biblioteca.juego_recomendable?(CarlosDuty)).to be true
+it "CarlosDuty es recomendable si no está en la Juegoteca" do
+  expect(Juegoteca.juego_recomendable?(CarlosDuty)).to be false
+  Juegoteca.borrar_juego!(CarlosDuty)
+  expect(Juegoteca.juego_recomendable?(CarlosDuty)).to be true
 end
 
-it "TimbaElLeon no es recomendable porque no es violento aunque no esté en la biblioteca" do
-  Biblioteca.borrar_juego!(TimbaElLeon)
-  expect(Biblioteca.juego_recomendable?(TimbaElLeon)).to be false
+it "TimbaElLeon no es recomendable porque no es violento aunque no esté en la Juegoteca" do
+  Juegoteca.borrar_juego!(TimbaElLeon)
+  expect(Juegoteca.juego_recomendable?(TimbaElLeon)).to be false
 end
