da88ef5fe4e9b2f32cad6df33e9c33be7b703b52
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-01 18:27:34 +0000

    Mumuki Export on 2021-02-01 18:27:34 +0000

--- 00006_Bloques con parámetros/extra.rb
+++ 00006_Bloques con parámetros/extra.rb
@@ -2,14 +2,20 @@ module TimbaElLeon
   @dificultad = 25
   
   def self.violento?
-    /*<elipsis-for-student@*/ false /*@elipsis-for-student>*/
+    /*<elipsis-for-student@*/ 
+    false
+    /*@elipsis-for-student>*/
   end
   
   def self.dificultad
-    /*<elipsis-for-student@*/ @dificultad /*@elipsis-for-student>*/
+    /*<elipsis-for-student@*/ 
+    @dificultad 
+    /*@elipsis-for-student>*/
   end
   
   def self.jugar!(un_tiempo)
-    /*<elipsis-for-student@*/ @dificultad += un_tiempo /*@elipsis-for-student>*/
+    /*<elipsis-for-student@*/ 
+    @dificultad += un_tiempo 
+    /*@elipsis-for-student>*/
   end
 end
