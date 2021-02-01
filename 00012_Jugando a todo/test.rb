373a39b87dc184eb935210311d0d699f6723b805
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-01 18:50:59 +0000

    Mumuki Export on 2021-02-01 18:50:59 +0000

--- 00012_Jugando a todo/test.rb
+++ 00012_Jugando a todo/test.rb
@@ -1,9 +1,9 @@
-it "Los juegos de la Biblioteca son CarlosDuty, TimbaElLeon y Metroide"  do
-  expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
+it "Los juegos de la Juegoteca son CarlosDuty, TimbaElLeon y Metroide"  do
+  expect(Juegoteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
 end
 
 it "Jugar a todo baja un punto la dificultad a CarlosDuty y sube en 5 la de TimbaElLeon"  do
-  Biblioteca.jugar_a_todo!
+  Juegoteca.jugar_a_todo!
   expect(CarlosDuty.dificultad).to eq 29.5
   expect(TimbaElLeon.dificultad).to eq 30
 end
