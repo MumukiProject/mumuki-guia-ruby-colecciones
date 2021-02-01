39fa4c0fc9b85f5fa400da95a75f7511b79752a4
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-01 18:41:38 +0000

    Mumuki Export on 2021-02-01 18:41:38 +0000

--- 00008_El que busca encuentra/test.rb
+++ 00008_El que busca encuentra/test.rb
@@ -1,15 +1,15 @@
-it "Los juegos de la Biblioteca son CarlosDuty, TimbaElLeon y Metroide"  do
-  expect(Biblioteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
+it "Los juegos de la Juegoteca son CarlosDuty, TimbaElLeon y Metroide"  do
+  expect(Juegoteca.juegos).to eq [CarlosDuty, TimbaElLeon, Metroide]
 end
 
 it "CarlosDuty es más difícil que 10"  do
-  expect(Biblioteca.juego_mas_dificil_que(10)).to eq CarlosDuty
+  expect(Juegoteca.juego_mas_dificil_que(10)).to eq CarlosDuty
 end
 
 it "Metroide es más difícil que 90"  do
-  expect(Biblioteca.juego_mas_dificil_que(90)).to eq Metroide
+  expect(Juegoteca.juego_mas_dificil_que(90)).to eq Metroide
 end
 
 it "Ningún juego es más difícil que 110"  do
-  expect(Biblioteca.juego_mas_dificil_que(110)).to eq nil
+  expect(Juegoteca.juego_mas_dificil_que(110)).to eq nil
 end
