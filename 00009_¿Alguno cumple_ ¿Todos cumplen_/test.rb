060df2ec14ede270fe40d047628908c2287e82f3
gustavo@mumuki.org <gustavo@mumuki.org>
2021-09-16 15:47:51 +0000

    Mumuki Export on 2021-09-16 15:47:51 +0000

--- 00009_¿Alguno cumple_ ¿Todos cumplen_/test.rb
+++ 00009_¿Alguno cumple_ ¿Todos cumplen_/test.rb
@@ -6,10 +6,6 @@ it "En la Juegoteca no hay mucha violencia porque TimbaElLeon no es violento"  d
   expect(Juegoteca.mucha_violencia?).to be false
 end
 
-it "En la Juegoteca hay mucha violencia si todos sus juegos son violentos"  do
-  expect(Juegoteca.mucha_violencia?).to be false
-end
-
 it "La Juegoteca es muy difícil porque CarlosDuty y Metroide tienen más de 25 puntos de dificultad"  do
   expect(Juegoteca.muy_dificil?).to eq true
 end
